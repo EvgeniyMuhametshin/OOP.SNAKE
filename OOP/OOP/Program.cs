0cfda3dab821e971882667a58043b70fbaf53409
Evgeniy <John090918@gmail.com>
2021-03-23 00:52:19 +0300

    List

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
@@ -11,6 +12,30 @@ namespace OOP
 
 			Point p2 = new Point(5, 4, '#');
 			p2.draw(p2.x, p2.y, p2.sym);
+
+			List<int> numList = new List<int>();//Класс Список
+			numList.Add(0);
+			numList.Add(1);
+			numList.Add(2);
+
+			//Обращение к значениям
+			int x = numList[0];
+			int y = numList[1];
+			int z = numList[2];
+
+			//Просмотреть все значения из списка
+			foreach (int i in numList)
+			{
+				Console.WriteLine(i);
+			}
+
+			//Удаление числа
+			numList.RemoveAt(0);
+
+			//Добавиль в лист
+			List <Point> pList = new List<Point>();
+			pList.Add(p1);
+			pList.Add(p2);
 
 			Console.ReadKey();
 		}
